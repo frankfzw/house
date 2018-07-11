d972c18a6231537b95c2cad32c3759926c813753
frankfzw <frankfzw@gmail.com>
2018-07-12 00:04:08 +0800

    get name and url

--- script.py
+++ script.py
@@ -1,12 +1,24 @@
 import requests
+from lxml import html
+import pandas as pd
 
-region_list = ['东方城市花园']
+region_list = ['东方城市花园', '中冶尚城']
 
 base_url = 'https://sh.lianjia.com/xiaoqu/rs'
+
+columns = ['name', 'year', 'school', 'price', 'size', 'url']
+
+query_result = []
+
 for r in region_list:
     postfix = '{}'.format(r.encode('utf-8'))[2:-1]
     postfix = postfix.replace('\\x', '%')
     postfix = postfix.upper()
     url = base_url + postfix
-    r = requests.post(url=url)
-    print(r.content)
+    search_result = requests.post(url=url)
+    tree = html.fromstring(search_result.content)
+    results = tree.xpath('//ul[@class="listContent"]/li[@class="clear xiaoquListItem"]')[0]
+    title = results.find_class('info')[0]
+    for link in title.xpath("//div[@class='title']//a[@target='_blank']"):
+        name = link.text_content()
+        href = link.attrib['href']
